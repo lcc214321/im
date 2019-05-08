3eff5ffeeea0601d17a79b9fc0aaa567981fb6c6
surpassm <lustres@sina.com>
2019-05-08 09:15:07 +0800

    first commit

---
+++ im-zk/src/main/java/com/github/surpassm/im/zk/ImZkApplication.java
@@ -0,0 +1,13 @@
+package com.github.surpassm.im.zk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImZkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImZkApplication.class, args);
+	}
+
+}
---
+++ im-zk/src/test/java/com/github/surpassm/im/zk/ImZkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.surpassm.im.zk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImZkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
